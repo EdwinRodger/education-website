9cb54256ec7e7f75bb86896bc7c6f671eddd844f
EdwinRodger <codevaidik@gmail.com>
2025-03-26 20:22:53 +0530

    add login and signup

--- app.py
+++ app.py
@@ -14,5 +14,15 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
